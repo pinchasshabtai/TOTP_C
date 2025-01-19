e871321c822ca5bee50b54e02d3e4d5e8ad46749
Pinchas <pinchas2812@gmail.com>
2025-01-19 02:38:22 +0200

    Creted TOTP Code

--- obj/Release/net8.0-windows/WpfApp2.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/WpfApp2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc6e9599d60ef18d722502bd03113f48340e209")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp2")]
